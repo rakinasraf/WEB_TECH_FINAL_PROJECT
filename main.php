5cf38effe880b142e8905e6915d993f8f103accf
rakinasraf <rakinasraf128@email.com>
2026-05-18 22:13:22 +0600

    modified home page

--- main.php
+++ main.php
@@ -3,18 +3,15 @@
 <head>
 <title>Home Page</title>
 <style>
-
 *{
     margin:0;
     padding:0;
     box-sizing:border-box;
     font-family: Arial, sans-serif;
 }
-
 body{
     background:#f4f6f9;
 }
-
 .navbar{
     width:100%;
     background:#1f2937;
@@ -22,13 +19,11 @@ body{
     text-align:center;
     box-shadow:0 2px 8px rgba(0,0,0,0.1);
 }
-
 .navbar h1{
     color:white;
     font-size:32px;
     letter-spacing:1px;
 }
-
 .container{
     width:450px;
     background:white;
@@ -38,14 +33,12 @@ body{
     box-shadow:0 4px 15px rgba(0,0,0,0.1);
     text-align:center;
 }
-
 .container p{
     font-size:20px;
     color:#333;
     margin-bottom:30px;
     font-weight:bold;
 }
-
 .container a{
     display:block;
     text-decoration:none;
@@ -57,11 +50,9 @@ body{
     font-size:17px;
     transition:0.3s;
 }
-
 .container a:hover{
     background:#1d4ed8;
 }
-
 </style>
 </head>
 <body>
@@ -73,6 +64,7 @@ Welcome to E-COMMERCE STORE<br><br>
 <a href="Admin/index.php">Enter As Admin</a><br><br>
 <a href="Seller/index.php">Enter As Seller</a><br><br>
 <a href="Customer/index.php">Enter As Customer</a><br><br>
+<a href="DeliveryManager/index.php">Enter As Delivery Manager</a><br><br>
 </div>
 </body>
 </html>
