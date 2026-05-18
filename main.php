003c5d1dc4f99afba0dd0344d681008158687585
rakinasraf <rakinasraf128@email.com>
2026-05-18 22:08:29 +0600

    modified home page

--- main.php
+++ main.php
@@ -73,7 +73,6 @@ Welcome to E-COMMERCE STORE<br><br>
 <a href="Admin/index.php">Enter As Admin</a><br><br>
 <a href="Seller/index.php">Enter As Seller</a><br><br>
 <a href="Customer/index.php">Enter As Customer</a><br><br>
-<a href="DeliveryManager/index.php">Enter As Delivery Manager</a><br><br>
 </div>
 </body>
 </html>
