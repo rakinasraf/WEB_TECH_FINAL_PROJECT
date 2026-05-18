484514c187bbe6787e26bbd6274f203aed0d38e2
rakinasraf <rakinasraf128@email.com>
2026-05-18 21:11:34 +0600

    Updated seller login page

--- Seller/Login.php
+++ Seller/Login.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Seller Login</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="navbar">
+<h2>Seller Login</h2>
+<a href="Register.php" class="btn btn-delete">Register</a>
+</div>
+<div class="container">
+<form action="AuthController.php" method="POST">
+    <input type="hidden" name="action" value="login">
+    Email:<br><input type="email" name="email"><br><br>
+    Password:<br><input type="password" name="password"><br><br>
+    <button type="submit">Login</button><br><br>
 <?php
-include("LoginView.php");
+session_start();
+if(isset($_SESSION['error']))
+{
+    echo "<p style='color:red'>";
+    echo $_SESSION['error'];
+    echo "</p>";
+    unset($_SESSION['error']);
+}
 ?>
+</form>
+</div>
+</body>
+</html>
