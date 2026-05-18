6aed2679cfd252e2032461eeaba7d3e77594133d
rakinasraf <rakinasraf128@email.com>
2026-05-19 02:10:19 +0600

    modify

--- main.php
+++ main.php
@@ -64,7 +64,7 @@ Welcome to E-COMMERCE STORE<br><br>
 <a href="Admin/index.php">Enter As Admin</a><br><br>
 <a href="Seller/index.php">Enter As Seller</a><br><br>
 <a href="Customer/index.php">Enter As Customer</a><br><br>
-<a href="DeliveryManager/index.php">Enter As Delivery Manager</a><br><br>
+<a href="Delivery Manager/public/index.php">Enter As Delivery Manager</a><br><br>
 </div>
 </body>
 </html>
