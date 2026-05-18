c82ae5bf8faf5d1cb8d1e96bf2abcdec3b014d19
rakinasraf <rakinasraf128@email.com>
2026-05-18 23:00:37 +0600

    modified admin database.php

--- Admin/config/database.php
+++ Admin/config/database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $db_name = "e-commerce_store";
+    private $db_name = "ecommercestore";
     private $username = "root";
     private $password = "";
     public $conn;
@@ -9,7 +9,7 @@ class Database {
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name,3307);
             if ($this->conn->connect_error) {
                 die("Connection failed: " . $this->conn->connect_error);
             }
