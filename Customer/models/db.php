0090c4280ba6880691a85d13ae9c567e6ca28244
rakinasraf <rakinasraf128@email.com>
2026-05-18 20:53:21 +0600

    Updated customer db.php page

--- Customer/models/db.php
+++ Customer/models/db.php
@@ -5,7 +5,7 @@ $user = "root";
 $pass = "";
 $db   = "ecommerce_store";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db,3307);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
