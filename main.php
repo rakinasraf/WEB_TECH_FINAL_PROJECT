52d741813e039a4b325ad7c05ee19b5b8084079b
rakinasraf <rakinasraf128@email.com>
2026-05-18 20:43:57 +0600

    Updated main page

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard</title>
+</head>
+<body>
+<div class="navbar">
+<h1>Home Page</h1>
+</div>
+<div class="container">
+Welcome to E-COMMERCE STORE<br><br>
+<a href="Admin/index.php">Enter As Admin</a><br><br>
+<a href="Seller/index.php">Enter As Seller</a><br><br>
+<a href="Customer/index.php">Enter As Customer</a><br><br>
+<a href="DeliveryManager/index.php">Enter As Delivery Manager</a><br><br>
+</div>
+</body>
+</html>
