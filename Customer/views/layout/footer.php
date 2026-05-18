2d2fdbf21409eb7f55e4050c2fadf5607ad4c074
saiyarajoyce <saiyarajoyce@gmail.com>
2026-05-18 18:35:57 +0600

    Add files via upload

---
+++ Customer/views/layout/footer.php
@@ -0,0 +1,8 @@
+</div><
+
+<footer style="text-align:center; padding:20px; margin-top:40px; background:#222; color:#aaa; font-size:13px;">
+    &copy; <?= date('Y') ?> ShopZone &mdash; All rights reserved.
+</footer>
+
+</body>
+</html>
